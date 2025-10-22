b14be6c47ff38b1e5ea64578155e650d57f2eff4
Andrespc <anfega@outlokk.es>
2025-10-21 22:53:53 -0500

    ADD: configuracion de firebase

---
+++ src/app/router/AppRouter.tsx
@@ -0,0 +1,21 @@
+// import { Routes, Route, Navigate } from 'react-router-dom';
+// import LoginPage from '../../presentation/pages/LoginPage';
+// import RegisterPage from '../../presentation/pages/RegisterPage';
+// import DashboardPage from '../../presentation/pages/DashboardPage';
+// import HabitsPage from '../../presentation/pages/HabitsPage';
+// import NotFoundPage from '../../presentation/pages/NotFoundPage';
+
+// const AppRouter = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Navigate to="/dashboard" />} />
+//       <Route path="/login" element={<LoginPage />} />
+//       <Route path="/register" element={<RegisterPage />} />
+//       <Route path="/dashboard" element={<DashboardPage />} />
+//       <Route path="/habits" element={<HabitsPage />} />
+//       <Route path="*" element={<NotFoundPage />} />
+//     </Routes>
+//   );
+// };
+
+// export default AppRouter;
